917cb54988003eeda6bb5564ab73495b05310818
Francisco.Capelo <fjcapelo@gmail.com>
2016-07-16 12:34:46 +0200

    Segundo

--- Escritorio/appinfo2/public/js/addCtrl.js
+++ Escritorio/appinfo2/public/js/addCtrl.js
@@ -1,15 +1,22 @@
-var addCtrl = angular.module('addCtrl', ['ngRoute']);
 
+var addCtrl = angular.module('addCtrl', ['ngRoute','jkuri.datepicker']);
 addCtrl.controller('addCtrl', function($scope, $http, $location){
 
     // Initializes Variables
     // ----------------------------------------------------------------------------
     $scope.formData = {};
      var self = this;
+     var la = "la";
+     var el = "el";
+     var asegur = "";
+
 
 
 
      $scope.createUser = function() {
+
+
+
        var danos = $scope.formData.danos;
        var causa = $scope.formData.causa;
        var origen = $scope.formData.origen;
@@ -26,8 +33,64 @@ addCtrl.controller('addCtrl', function($scope, $http, $location){
         var circunstancias4 =  $scope.formData.circunstancias4;
         var circunstancias5 =  $scope.formData.circunstancias5;
           var reserva =  $scope.formData.reserva;
+          var causadeclarada =  $scope.formData.causadeclarada;
+          var cia =  $scope.formData.cia;
+          var fechaencargo =  $scope.formData.fechaencargo;
+          var fechacontacto = $scope.formData.fechacontacto;
+          var fechavisita =  $scope.formData.fechavisita;
+          var horavisita =  $scope.formData.horavisita;
+          var tiposiniestro =  $scope.formData.tiposiniestro;
+          var asegur = $scope.formData.asegur;
+          var riesgotipo = $scope.formData.riesgotipo;
+          var utilidad = $scope.formData.utilidad;
+          var protecciones = $scope.formData.protecciones;
+          var construccion = $scope.formData.construccion;
+          var antiguedad = $scope.formData.antiguedad;
+          var estado = $scope.formData.estado;
+          var superficie = $scope.formData.superficie;
+          var recomendaciones = $scope.formData.recomendaciones;
+          var garantia1 = $scope.formData.garantia1;
+          var garantia2 = $scope.formData.garantia2;
+          var capitalAsegurado = $scope.formData.capitalAsegurado;
+          var capitalAsegurado2 = $scope.formData.capitalAsegurado2;
+          var capitalPreexistencia = $scope.formData.capitalPreexistencia;
+          var capitalPreexistencia2 = $scope.formData.capitalPreexistencia2;
+          var valorm2 = $scope.formData.valorm2;
 
-     $location.url('/resultado/' + danos + '/' + causa + '/' + origen + '/' + vivienda + '/' + zona + '/' + don + '/' + nombre+ '/' + cobertura + '/' + garantia + '/' + riesgo + '/' + circunstancias + '/' + circunstancias2 + '/' + circunstancias3 + '/' + circunstancias4 + '/' + circunstancias5+ '/' + reserva);
+          var genero1 = "pp";
+
+       genero(asegur);
+
+         function genero (asegur){
+
+          if (asegur == "el Asegurado, D."){
+            genero1 = "el";
+
+          } else if ( asegur == "la Asegurada, Dña."){
+            genero1 = "la";
+                  }
+                else if ( asegur == "la gerente de la empresa asegurada, Dña."){
+                  genero1 = "la";}
+               else if ( asegur == "el gerente de la empresa asegurada, D."){
+                  genero1 = "el";}
+                  else if ( asegur == "el encargado, D."){
+                     genero1 = "el";}
+                     else if ( asegur == "la encargada, Dña."){
+                        genero1 = "la";}
+                  else {}
+
+
+        };
+
+
+
+
+     $location.url('/resultado/' + danos + '/' + causa + '/' + origen + '/' + vivienda + '/' + zona + '/' + don + '/'
+     + nombre+ '/' + cobertura + '/' + garantia + '/' + riesgo + '/' + circunstancias + '/' + circunstancias2 + '/'
+     + circunstancias3 + '/' + circunstancias4 + '/' + circunstancias5 + '/' + reserva+ '/' + causadeclarada + '/'
+     + cia + '/' + fechaencargo + '/' + fechacontacto + '/' + fechavisita + '/' + horavisita + '/' + tiposiniestro + '/'
+      + asegur + '/' + genero1 + '/' + riesgotipo + '/' + utilidad + '/' + protecciones + '/' + construccion  + '/' + antiguedad + '/'
+       + estado + '/' + superficie + '/' + recomendaciones + '/' + garantia1 + '/' + garantia2 + '/' + capitalAsegurado + '/' + capitalAsegurado2 + '/' + capitalPreexistencia + '/' + capitalPreexistencia2 + '/' + valorm2);
 }
 
 } );
--- Escritorio/appinfo2/public/js/control-parametros.js
+++ Escritorio/appinfo2/public/js/control-parametros.js
@@ -76,3 +76,165 @@ addCtrl.controller('don', ['$scope', function($scope) {
    ],
   };
 }]);
+
+addCtrl.controller('circunstancias2', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Que en fecha de siniestro hubo un corte en el suministro eléctrico sobre las {{formData.hora}}, que afecto al {{formData.danos}}.', name: 'Corte de suministro'},
+     {id: 'Que en fecha de siniestro, y sin motivo aparente, dejo de funcionar el {{formData.danos}}.', name: 'Sin motivo aparente'},
+     {id: 'Que en fecha de siniestro, fueron a utilizar el {{formData.danos}}, y observaron que no se ponía en marcha.', name: 'No se pone en marcha'},
+       {id: 'Que posterior a varios cortes de suministro eléctrico sobre las {{formData.hora}} , dejó de funcionar el {{formData.danos}}', name: 'Varios cortes'}
+
+   ],
+  };
+}]);
+
+addCtrl.controller('pieza', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'el motor eléctrico del compresor', name: 'Motor compresor'},
+     {id: 'el bobinado del motor', name: 'Bobinado de motor'},
+     {id: 'la fuente de alientación', name: 'Fuente de alimentación'},
+       {id: 'la placa de control', name: 'Placa de control'}
+
+   ],
+  };
+}]);
+
+addCtrl.controller('circunstancias3', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Avisaron a un servicio técnico particularmente, el cual se desplazó al riesgo y tras comprobar el {{formData.danos}}, localizó daños en {{formData.pieza}}.', name: 'Avisaron a un servicio'},
+     {id: 'Indica que hay más afectados en la zona.', name: 'Mas afectados'},
+     {id: 'No ha avisado al servicio técnico, por que estaba a la espera de valoración por parte de su compañía aseguradora.', name: 'No avisa a servicio'}
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('circunstancias4', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Se han desecho del aparato afectado.', name: 'Se han desecho'},
+     {id: '"El aparato se lo ha llevado el servicio técnico.', name: 'El servicio se lleva pieza'},
+     {id: 'No ha avisado al servicio técnico, por que estaba a la espera de valoración por parte de su compañía aseguradora.', name: 'No avisa a servicio'}
+
+
+   ],
+  };
+}]);
+
+
+addCtrl.controller('circunstancias5', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Intento poner en funcionamiento el aparato, comprobando que no se pone en funcionamiento.', name: 'No funciona'},
+     {id: '"El Asegurado muestra la pieza sustituida. Compruebo que sufre daños de origen eléctrico en su etapa primaria', name: 'Muestra  pieza'}
+
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('utilidad', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Principal', name: 'Principal'},
+     {id: 'Secundaria', name: 'Secundaria'},
+     {id: 'Restaurante', name: 'Restaurante'}
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('protecciones', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Puerta blindada y rejas en ventanas a menos de 3 metros del suelo', name: 'Puerta blin y rejas'},
+     {id: 'Puerta maciza', name: 'Maciza'},
+     {id: 'Puerta blindada', name: 'Blindada'},
+     {id: 'Puerta blindada y alarma', name: 'Blindada y alarma'},
+     {id: 'Las que constan en póliza', name: 'Las que constan'},
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('construccion', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Solidad de ladrillo', name: 'Ladrillo'},
+     {id: 'Solida de bloque', name: 'Bloque'},
+     {id: 'Madera', name: 'Madera'},
+       {id: 'Solidad de hormigón', name: 'Solida de hormigón'}
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('estado', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Buendo', name: 'Bueno'},
+     {id: 'Falta de mantenimiento', name: 'Falta mantenimiento'},
+     {id: 'Malo', name: 'Malo'}
+
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('garantia1', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Mobiliario', name: 'Mobiliario'},
+     {id: 'Contenido', name: 'Contenido'}
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('garantia2', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'Edificación', name: 'Edificiación'},
+     {id: 'Continente', name: 'Continente'}
+
+
+   ],
+  };
+}]);
+
+addCtrl.controller('articulo', ['$scope', function($scope) {
+  $scope.data = {
+   model: null,
+   availableOptions: [
+     {id: 'FENOMENOS ELECTRICOS', name: 'Fenómenos eléctricos'},
+     {id: 'DAÑOS ELETRICOS', name: 'Daños eléctricos'},
+      {id: 'AVERIA DE MAQUINARIA', name: 'Avería de maquinaria'},
+       {id: 'ROTURA', name: 'Rotura'},
+        {id: 'INCENDIO', name: 'Incendio'},
+         {id: ' AGUA', name: 'Agua'}
+
+
+   ],
+  };
+}]);
